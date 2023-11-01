c77c729b2dd15f31309876254ce846b04a2e5111
gorilskij <43991400@users.noreply.github.com>
2023-11-01 15:51:39 +0100

    implement iter_from

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,27 @@ extern crate self as enum_rotate;
 
 pub use derive_enum_rotate::EnumRotate;
 
+pub struct Iter<E>(Vec<E>);
+
+impl<E> Iter<E> {
+    pub fn new(vec: Vec<E>) -> Self {
+        Self(vec)
+    }
+}
+
+impl<E> Iterator for Iter<E> {
+    type Item = E;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 pub trait EnumRotate where Self: Sized + Copy {
+    #[must_use]
     fn next(self) -> Self;
 
+    #[must_use]
     fn prev(self) -> Self;
 
     fn rotate_next(&mut self) -> Self {
@@ -16,4 +34,8 @@ pub trait EnumRotate where Self: Sized + Copy {
         *self = self.prev();
         *self
     }
+
+    fn iter() -> Iter<Self>;
+
+    fn iter_from(self) -> Iter<Self>;
 }
--- tests/rotate.rs
+++ tests/rotate.rs
@@ -5,6 +5,9 @@ enum Test {
     A, B, C
 }
 
+#[derive(EnumRotate, Copy, Clone, Eq, PartialEq, Debug)]
+enum Void {}
+
 #[test]
 fn test_next_prev() {
     assert_eq!(Test::A.next(), Test::B);
@@ -41,4 +44,17 @@ fn test_rotate() {
     let mut x = Test::C;
     assert_eq!(x.rotate_prev(), Test::B);
     assert_eq!(x, Test::B);
+}
+
+#[test]
+fn test_iter() {
+    assert_eq!(Test::iter().collect::<Vec<_>>(), vec![Test::A, Test::B, Test::C]);
+    assert_eq!(Void::iter().collect::<Vec<_>>(), vec![]);
+}
+
+#[test]
+fn test_iter_from() {
+    assert_eq!(Test::A.iter_from().collect::<Vec<_>>(), vec![Test::A, Test::B, Test::C]);
+    assert_eq!(Test::B.iter_from().collect::<Vec<_>>(), vec![Test::B, Test::C, Test::A]);
+    assert_eq!(Test::C.iter_from().collect::<Vec<_>>(), vec![Test::C, Test::A, Test::B]);
 }
